9816a7c75dd778c51bdf3e4f75b0e9f980b2299c
Carlos Ruiz <caroru1974@gail.com>
2023-12-27 11:25:18 -0500

    import error fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Header from "../sections/header"
+import Header from "../sections/Header"
 import FirstSection from "../sections/FirstSection"
 import Footer from "../sections/Footer"
 
