33975087003d297633461ed5689c1c2ef3daeb1c
Carlos Ruiz <caroru1974@gail.com>
2023-12-27 11:22:50 -0500

    import error fixed

--- src/pages/[tab]/[subsection].js
+++ src/pages/[tab]/[subsection].js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import Layout from "../../components/layout"
-import Header from "../../sections/header"
+import Header from "../../sections/Header"
 import Footer from "../../sections/Footer"
 import styled from "styled-components"
 import pages from "../../data/pages.json"
@@ -14,53 +14,53 @@ import QuoteSection from "../../sections/QuoteSection"
 import CardsSection from "../../sections/CardsSection"
 
 
-const SubSectionPage = () => {   
-    const location=useLocation()
+const SubSectionPage = () => {
+    const location = useLocation()
     const pathname = location.pathname
     const pathParts = pathname.split("/");
     const lastPathPart = pathParts[pathParts.length - 2]
 
-    const page=pages.find(page => encodeURIComponent(page.title) === lastPathPart)
+    const page = pages.find(page => encodeURIComponent(page.title) === lastPathPart)
 
     if (!page) {
         return (
             <Layout>
-                <Header isIndex={false}/>
-                <NotFoundPage />       
+                <Header isIndex={false} />
+                <NotFoundPage />
                 <Footer />
             </Layout>
         )
     }
-        
+
     return (
-    <Layout>
-        <Header isIndex={false}/>
-        <TopBannerContainer>            
-            <GradientText>{page.title}</GradientText>
-            <SubTitle>{page.subtitle}</SubTitle>
-            {page.firstline && <p>{page.firstline}</p>}
-            {page.text && <p>{page.text}</p>} 
-            {page.buttons && 
-                <ButtonContainer>
-                    {page.buttons.map(button => <Button variant="primary" key={button}>{button}</Button>)}
-                </ButtonContainer>}
-        </TopBannerContainer> 
-        {page.videosection &&
-            <VideoSectionContainer>
-                <VideoSection title={page.videosection.title}  URL={page.videosection.videoURL} text={page.videosection.text}/>
-            </VideoSectionContainer>
-        }
-        {page.clientscarousel && <ClientsCarousel /> }
-        {page.quotessection && <QuoteSection quote={page.quotessection}></QuoteSection>}
-        {page.cardssection && <CardsSection cards={page.cardssection}></CardsSection>}
-    
-        <InsightsCarousel />
-        <Footer />
-    </Layout>
+        <Layout>
+            <Header isIndex={false} />
+            <TopBannerContainer>
+                <GradientText>{page.title}</GradientText>
+                <SubTitle>{page.subtitle}</SubTitle>
+                {page.firstline && <p>{page.firstline}</p>}
+                {page.text && <p>{page.text}</p>}
+                {page.buttons &&
+                    <ButtonContainer>
+                        {page.buttons.map(button => <Button variant="primary" key={button}>{button}</Button>)}
+                    </ButtonContainer>}
+            </TopBannerContainer>
+            {page.videosection &&
+                <VideoSectionContainer>
+                    <VideoSection title={page.videosection.title} URL={page.videosection.videoURL} text={page.videosection.text} />
+                </VideoSectionContainer>
+            }
+            {page.clientscarousel && <ClientsCarousel />}
+            {page.quotessection && <QuoteSection quote={page.quotessection}></QuoteSection>}
+            {page.cardssection && <CardsSection cards={page.cardssection}></CardsSection>}
+
+            <InsightsCarousel />
+            <Footer />
+        </Layout>
     )
 }
 
-const TopBannerContainer=styled.div`
+const TopBannerContainer = styled.div`
     display: flex;
     flex-direction: column;
     padding: 100px 70px 100px 100px;
@@ -78,20 +78,20 @@ const GradientText = styled.p`
     word-wrap: break-word;
 `
 
-const SubTitle=styled.p`
+const SubTitle = styled.p`
     font-size: 50px;
     padding:0;
     margin:0;
 `
 
-const ButtonContainer=styled.div`
+const ButtonContainer = styled.div`
     display: flex;
     flex-direction: row;
     width: 35%;
     justify-content: space-between;
 `
 
-const VideoSectionContainer=styled.div`
+const VideoSectionContainer = styled.div`
     display: flex;
     height: 450px;
     justify-content: center;
